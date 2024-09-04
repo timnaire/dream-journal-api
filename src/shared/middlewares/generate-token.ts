bb6398e5c3b759702717def527bec27076d9c33c
timnaire <timmydonaire@gmail.com>
2024-09-04 12:34:23 +0800

    Change token expiry and added nodemailer for forgot password implementation

--- src/shared/middlewares/generate-token.ts
+++ src/shared/middlewares/generate-token.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (username: string, password: string) => {
-  return jwt.sign({ username, password }, process.env.JWT_SECRET as string, { expiresIn: "1800s" });
+  return jwt.sign({ username, password }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
 };
